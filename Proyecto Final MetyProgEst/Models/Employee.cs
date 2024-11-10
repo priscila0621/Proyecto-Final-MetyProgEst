a629ddddcdeb0a165b52dd9d465c39eb05e02c23
priscila0621 <pjulieths@uamv.edu.ni>
2024-11-10 17:00:08 -0600

    Se creó la clase "Employee"

---
+++ Proyecto Final MetyProgEst/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_MetyProgEst.Models
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public decimal BaseSalary { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
